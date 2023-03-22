e67a4dfc39996582eac558e837627106dde5cbb3
acoexo <acseoyo04@gmail.com>
2023-03-22 14:41:05 +0000

    v7.2

---
+++ js/js.js
@@ -0,0 +1,48 @@
+const formulario=document.querySelector(".form");
+const datos = {
+    nombre:"",
+    email:"",
+    mensaje:""
+};
+
+nombre.addEventListener('input', leerTexto);
+email.addEventListener('input', leerTexto);
+mensaje.addEventListener('input', leerTexto);
+
+function leerTexto(e){
+    console.log(e);
+    datos[e.target.id]=e.target.value;
+};
+
+formulario.addEventListener('click', function(e){
+    e.preventDefault();
+    const nombre=datos.nombre;
+    const email=datos.email;
+    const mensaje= datos.mensaje;
+    if(nombre==="" || email==="" ||mensaje===""){
+        console.log("Al menos hay un campo vacío");
+        mostrarError("Todos los campos son obligatorios");
+        return;
+    }else{
+        console.log("Se han enviado todos los datos correctamente")
+        mostrarMensaje("Se ha enviado correctamente");
+        return;
+    }
+});
+
+function mostrarError(mensaje){
+    const alerta=document.createElement("p");
+    alerta.textContent=mensaje;
+    alerta.classList.add("red");
+    formulario.appendChild(alerta);
+    setTimeout(()=>{alerta.remove()}, 1000);
+
+}
+function mostrarMensaje(mensaje){
+    const correct=document.createElement("p");
+    correct.textContent=mensaje;
+    correct.classList.add("grin");
+    formulario.appendChild(correct);
+    setTimeout(()=>{correct.remove()}, 1000);
+
+}
